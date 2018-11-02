ebd7d904f4ee30fc5ff82b9364832c025f146850
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-01 21:09:55 -0500

    Edit config.json, burger.js for sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    // Giving the Burger model a burger_name of type STRING
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 30]
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
 
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll(function(res) {
-      cb(res);
-    });
-  },
-  insertOne: function(burger_name, cb) {
-    orm.insertOne(burger_name, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(id, cb) {
-    orm.updateOne(id, function(res) {
-      cb(res);
-    });
-  }
-}
+  });
 
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
+  return Burger;
+};
